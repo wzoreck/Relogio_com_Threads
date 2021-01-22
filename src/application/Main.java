af8f52214528568cc02268b9586bcdc94cba35c5
Daniel Wzoreck <wzoreck2000@gmail.com>
2021-01-22 11:46:34 -0300

    Init main menu

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,14 +1,78 @@
 package application;
 
+import java.util.Scanner;
+
+import threads.Alarm;
+import threads.Data;
+import threads.Stopwatch;
 import threads.Time;
 
 public class Main {
 
 	public static void main(String[] args) {
-		Time time = new Time();
-		time.start();
-		for (int i = 0; i < 1000; i++)
-			System.out.println("Thread main");
+
+		Data data = new Data();
+
+		// Threads
+		Time time = new Time(data);
+		Stopwatch stopwatch = new Stopwatch(data);
+		Alarm alarm = new Alarm(data);
+
+		Scanner sc = new Scanner(System.in);
+
+		boolean runApplication = true;
+
+		while (runApplication) {
+			int menuChoice;
+
+			System.out.println();
+			System.out.println("--------------- Menu ---------------");
+			System.out.println("[1]  - Adjust time");
+			System.out.println("[2]  - View schedule");
+			System.out.println("[3]  - Start timer");
+			System.out.println("[4]  - Stop timer");
+			System.out.println("[5]  - Restart timer");
+			System.out.println("[6]  - Set alarm");
+			System.out.println("[99] - End the program");
+			System.out.println("------------------------------------");
+			System.out.print("Enter your choice: ");
+			menuChoice = sc.nextInt();
+			System.out.println("------------------------------------");
+
+			switch (menuChoice) {
+			case 1:
+
+				break;
+			case 2:
+
+				break;
+			case 3:
+
+				break;
+			case 4:
+
+				break;
+			case 5:
+
+				break;
+			case 6:
+
+				break;
+			case 99:
+				runApplication = false;
+				break;
+
+			default:
+				System.out.println();
+				System.out.println("The chosen option is not available!");
+				break;
+			}
+		}
+
+		sc.close();
+		System.out.println();
+		System.out.println("*Program ended*");
+
 	}
 
 }
